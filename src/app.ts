ff6e92db6804142d55329bfa2a212e1cbe1456a9
BuGalter <wardoc78@gmail.com>
2023-11-07 13:54:35 +0300

    feat: Added to app code for app delete: Delete start test code

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,8 @@
 import * as Phaser from 'phaser';
+import { MainScene, LoadingScene, AuthorScene } from './scenes';
+import { configPhaser } from './config';
+import { addScenesToConfig } from './utils';
 
-const screenHeight = window.innerHeight;
-const screenWidth = window.innerWidth;
+configPhaser.scene = addScenesToConfig(LoadingScene, MainScene, AuthorScene);
 
-export default class Demo extends Phaser.Scene {
-  constructor() {
-    super('demo');
-  }
-
-  preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
-    this.load.image('libs', 'assets/libs.png');
-  }
-
-  create() {
-    this.add.image(screenWidth / 2, screenHeight / 2, 'libs');
-
-    const logo = this.add.image(screenWidth / 2, 70, 'logo');
-
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1,
-    });
-  }
-}
-
-const config = {
-  type: Phaser.AUTO,
-  backgroundColor: '#125555',
-  width: screenWidth,
-  height: screenHeight,
-  scene: Demo,
-};
-
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(configPhaser);
